d22ea9c38b4984d4ec54256e61797ef1668f1174
yg <rjafydrkr@gmail.com>
2018-10-30 09:56:16 +0900

    234

--- js/jquery/portfolio.js
+++ js/jquery/portfolio.js
@@ -155,4 +155,13 @@ $(document).ready(function(){
     $('.sendBtn').click(function(){
         alert('서비스 준비중입니다.')
     })
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5bd7a9bc476c2f239ff68306/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
 })
